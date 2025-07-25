1338b1d8b8c14f603a9dbed8933261b4a0192088
Darshil Mukesh Desai <darshil.desai.201083@gmail.com>
2025-07-25 13:23:22 -0400

    bento grid edited

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -115,7 +115,7 @@ const About = () => {
                 <div className="xl:col-span-1 xl:row-span-2">
                     <div className="grid-container">
                         <img src="/assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[126px] h-fit object-cover sm:object-top" />
-                        <div className="mt-6">
+                        <div>
                             <p className="grid-subtext text-center">
                                 Contact Me
                             </p>
