d103ddbad096307cecbe80bff69b26279c0cac48
Darshil Mukesh Desai <darshil.desai.201083@gmail.com>
2025-11-23 16:10:42 -0500

    Conditional rendering of components on mobile devices

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,6 @@ import {HackerRoom} from "../components/HackerRoom.jsx";
 import CanvasLoader from "../components/CanvasLoader.jsx";
 import {useMediaQuery} from "react-responsive";
 import {calculateSizes} from "../constants/index.js";
-import Target from "../components/Target.jsx";
 import ReactLogo from "../components/ReactLogo.jsx";
 import Cube from "../components/Cube.jsx";
 import Rings from "../components/Rings.jsx";
@@ -47,7 +46,6 @@ const Hero = () => {
                     </Suspense>
                     {!isMobile && !isSmall && (
                         <group>
-                            <Target position = {sizes.targetPosition}/>
                             <ReactLogo position = {sizes.reactLogoPosition}/>
                             <Cube position = {sizes.cubePosition}/>
                             <Rings position = {sizes.ringPosition}/>
